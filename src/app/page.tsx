3fd027eee28701d03e7113a94730445d7cd3b66a
ingleshivam <ingleshivam@gmail.com>
2024-08-08 09:43:39 +0530

    firstt3app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,38 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
+async function Image(){
   const images = await db.query.images.findMany({
     orderBy:(model, {desc}) => desc(model.id),
   });
+  return(
+    <div className="flex flex-wrap gap-4">
+    {
+      [...images, ...images, ...images].map((image, index)=>(
+        <div key={image.id +"-"+index} className="flex w-48 flex-col">
+          <img src={image.url}/>
+          <div>{image.name}</div>
+        </div>
+      ))
+    }
+  </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {
-          [...images, ...images, ...images].map((image, index)=>(
-            <div key={image.id +"-"+index} className="flex w-48 flex-col">
-              <img src={image.url}/>
-              <div>{image.name}</div>
-            </div>
-          ))
-        }
-      </div>
-      Hello (gallary in progress)
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">
+          Please SignIn Above...
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Image/>
+      </SignedIn>
     </main>
   );  
 }
