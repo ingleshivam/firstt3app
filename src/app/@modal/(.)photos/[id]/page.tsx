acd36386adba6a5e206b89d42fd90455442147d3
ingleshivam <ingleshivam@gmail.com>
2024-08-08 14:11:09 +0530

    Parallel Processing

--- src/app/@modal/(.)photos/[id]/page.tsx
+++ src/app/@modal/(.)photos/[id]/page.tsx
@@ -1,12 +1,14 @@
-import { getImage } from "~/server/queries";
 
-export default async function PhotoModal({
+import { Modal } from "./modal";
+import FullPageImageView from "~/components/full-image-page";
+
+export default function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
   const idAsNumber  = Number(photoId);
   if(Number.isNaN(idAsNumber)) throw new Error("Invalid Photo Id");
-  const image = await getImage(idAsNumber);
-  return <div><img src={image.url} className="w-96"/></div>;
+
+  return <Modal><FullPageImageView id={idAsNumber}/></Modal>;
 }
