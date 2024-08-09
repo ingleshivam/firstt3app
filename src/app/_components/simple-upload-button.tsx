2ac1b0e103f103de7bc1d82a320652d63ce43e98
ingleshivam <ingleshivam@gmail.com>
2024-08-09 10:12:55 +0530

    Shadcn/UI

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -1,6 +1,9 @@
+
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "../utils/uploadthing";
 import { toast } from "sonner"
+import { LoadingSpinnerSVG } from "../page";
+import { useEffect } from "react";
 
 type input = Parameters<typeof useUploadThing>;
 
@@ -34,16 +37,71 @@ function UploadSVG(){
     );
   }
 
+// function makeUploadToast(){
+//    return toast(
+//         <div className="flex items-center">
+//                     <span>
+//                         <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+//                             <path 
+//                             d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" 
+//                             opacity=".25"/>
+//                             <path 
+//                             d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" 
+//                             className="spinner_ajPY"
+//                             fill="white"/>
+//                         </svg>
+//                     </span>
+//                     <span className="ms-2">
+//                         Uploading....
+//                     </span>
+//         </div>,
+//         {
+//         duration:5000,
+//         id:"upload-begin",
+//         }
+//     );
+// }
+
+// window.makeToast = makeUploadToast; 
+
 export function SimpleUploadButton(){
 
     //  const {inputProps} = useUploadThingInputProps("imageUploader");
     const router= useRouter();
+
+    // useEffect(()=>{
+    //     toast("Hi",
+    //         {
+    //         duration:5000,
+    //         id:"upload-begin",
+    //         }
+    //     );
+    // },[])
+
      const {inputProps} = useUploadThingInputProps("imageUploader",{
         onUploadBegin(){
-            toast("Uploading...",{
+            toast(
+                <div className="flex items-center">
+                <span>
+                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path 
+                        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" 
+                        opacity=".25"/>
+                        <path 
+                        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" 
+                        className="spinner_ajPY"
+                        fill="white"/>
+                    </svg>
+                </span>
+                <span className="ms-2">
+                    Uploading....
+                </span>
+    </div>,
+                {
                 duration:5000,
                 id:"upload-begin",
-            });
+                }
+            );
         },
         onClientUploadComplete(){
             toast.dismiss("upload-begin");
